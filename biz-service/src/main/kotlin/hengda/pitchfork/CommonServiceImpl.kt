b2e2dc7e0c24f36d1147b68cc09072f57a22a6df
ovaphlow <ovaphlow@outlook.com>
2019-12-03 14:20:52 +0800

    fixed #12

--- biz-service/src/main/kotlin/hengda/pitchfork/CommonServiceImpl.kt
+++ biz-service/src/main/kotlin/hengda/pitchfork/CommonServiceImpl.kt
@@ -209,35 +209,4 @@ class CommonServiceImpl: CommonGrpc.CommonImplBase() {
         responseObserver.onNext(reply)
         responseObserver.onCompleted()
     }
-
-    override fun save(req: CommonRequest, responseObserver: StreamObserver<CommonReply>) {
-        val gson = Gson()
-        val resp: MutableMap<String, Any> = mutableMapOf("message" to "", "content" to "")
-
-        try {
-            val conn = DBUtil.getConn()
-            val sql: String = """
-                insert into
-                    public.train (master_id, name, remark)
-                    values (?, ?, ?)
-                returning id
-            """.trimIndent()
-            val ps = conn.prepareStatement(sql)
-            val body = gson.fromJson(req.data.toString(), Map::class.java);
-            val id = body.get("master_id").toString().toDouble().toInt()
-            ps.setInt(1, id)
-            ps.setString(2, body.get("name").toString())
-            ps.setString(3, body.get("remark").toString())
-            val rs = ps.executeQuery()
-            resp.put("content", DBUtil.getMap(rs))
-            conn.close()
-        } catch (e: Exception) {
-            e.printStackTrace()
-            resp.put("message", "gRPC服务器错误")
-        }
-
-        val reply = CommonReply.newBuilder().setData(gson.toJson(resp)).build()
-        responseObserver.onNext(reply)
-        responseObserver.onCompleted()
-    }
 }
